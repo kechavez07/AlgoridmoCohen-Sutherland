317c2286df7772dbfe38e30ed99221d57465fc21
Kleber Enrique Chavez Oscullo <kleberenriqueos@gmail.com>
2025-06-17 12:51:08 -0500

    listo

--- Form1.cs
+++ Form1.cs
@@ -7,108 +7,138 @@ namespace AlgoridmoCohen_Sutherland
 {
     public partial class Form1 : Form
     {
+        // Variables para la línea
         private List<Point> puntos = new List<Point>();
-        private Bitmap canvas;
+
+        // Ventana de recorte (puedes cambiar estos valores)
+        private Rectangle rectRecorte = new Rectangle(100, 80, 300, 200); // x, y, ancho, alto
+
 
         public Form1()
         {
             InitializeComponent();
-            this.Load += Form1_Load;
+            pictureBox1.Paint += pictureBox1_Paint;
             pictureBox1.MouseDown += pictureBox1_MouseDown;
         }
+        // Códigos de región (outcode)
+        const int INSIDE = 0; // 0000
+        const int LEFT = 1;   // 0001
+        const int RIGHT = 2;  // 0010
+        const int BOTTOM = 4; // 0100
+        const int TOP = 8;    // 1000
 
-        private void Form1_Load(object sender, EventArgs e)
+        // Calcular código de región
+        private int CalcularCodigo(Point p)
         {
-            // Crear el canvas solo una vez al iniciar
-            canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            pictureBox1.Image = canvas;
-            DibujarCuadricula();
-        }
+            int code = INSIDE;
 
+            if (p.X < rectRecorte.Left)
+                code |= LEFT;
+            else if (p.X > rectRecorte.Right)
+                code |= RIGHT;
+
+            if (p.Y < rectRecorte.Top)
+                code |= TOP;
+            else if (p.Y > rectRecorte.Bottom)
+                code |= BOTTOM;
+
+            return code;
+        }
+        private bool CohenSutherland(Point p1, Point p2, out Point rec1, out Point rec2)
+        {
+            int xMin = rectRecorte.Left;
+            int xMax = rectRecorte.Right;
+            int yMin = rectRecorte.Top;
+            int yMax = rectRecorte.Bottom;
+
+            int code1 = CalcularCodigo(p1);
+            int code2 = CalcularCodigo(p2);
+
+            bool accept = false;
+
+            rec1 = p1;
+            rec2 = p2;
+
+            while (true)
+            {
+                if ((code1 | code2) == 0)
+                {
+                    // Ambos dentro: Aceptar
+                    accept = true;
+                    break;
+                }
+                else if ((code1 & code2) != 0)
+                {
+                    // Ambos fuera y en la misma región: Rechazar
+                    break;
+                }
+                else
+                {
+                    // Alguno afuera: recortar
+                    int codeOut = code1 != 0 ? code1 : code2;
+                    int x = 0, y = 0;
+
+                    if ((codeOut & TOP) != 0)
+                    {
+                        x = p1.X + (p2.X - p1.X) * (yMin - p1.Y) / (p2.Y - p1.Y);
+                        y = yMin;
+                    }
+                    else if ((codeOut & BOTTOM) != 0)
+                    {
+                        x = p1.X + (p2.X - p1.X) * (yMax - p1.Y) / (p2.Y - p1.Y);
+                        y = yMax;
+                    }
+                    else if ((codeOut & RIGHT) != 0)
+                    {
+                        y = p1.Y + (p2.Y - p1.Y) * (xMax - p1.X) / (p2.X - p1.X);
+                        x = xMax;
+                    }
+                    else if ((codeOut & LEFT) != 0)
+                    {
+                        y = p1.Y + (p2.Y - p1.Y) * (xMin - p1.X) / (p2.X - p1.X);
+                        x = xMin;
+                    }
+
+                    if (codeOut == code1)
+                    {
+                        p1 = new Point(x, y);
+                        code1 = CalcularCodigo(p1);
+                        rec1 = p1;
+                    }
+                    else
+                    {
+                        p2 = new Point(x, y);
+                        code2 = CalcularCodigo(p2);
+                        rec2 = p2;
+                    }
+                }
+            }
+            return accept;
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Dibujar área de recorte (ventana)
+            e.Graphics.DrawRectangle(Pens.Blue, rectRecorte);
+
+            // Dibujar línea original
+            if (puntos.Count == 2)
+            {
+                e.Graphics.DrawLine(Pens.Gray, puntos[0], puntos[1]);
+
+                // Recorte
+                if (CohenSutherland(puntos[0], puntos[1], out Point r1, out Point r2))
+                {
+                    e.Graphics.DrawLine(new Pen(Color.Red, 2), r1, r2); // Línea recortada en rojo
+                }
+            }
+        }
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            // Limitar a dos puntos para la línea
-            if (puntos.Count == 3)
+            if (puntos.Count == 2)
                 puntos.Clear();
 
             puntos.Add(e.Location);
-            Dibujarvector();
-        }
-
-        private void Dibujarvector()
-        {
-            using (Graphics g = Graphics.FromImage(canvas))
-            {
-                // 2) Dibujar puntos seleccionados
-                DibujarPuntos(g);
-
-                // 3) Dibujar línea entre los puntos (si hay dos)
-                DibujarLinea(g);
-            }
-
-            // Refrescar PictureBox
-            pictureBox1.Image = canvas;
-        }
-
-        //private void DibujarTodo()
-        //{
-        //    using (Graphics g = Graphics.FromImage(canvas))
-        //    {
-        //        g.Clear(Color.White);
-
-        //        // 1) Dibujar cuadrícula
-        //        DibujarCuadricula(g);
-
-        //        // 2) Dibujar puntos seleccionados
-        //        DibujarPuntos(g);
-
-        //        // 3) Dibujar línea entre los puntos (si hay dos)
-        //        DibujarLinea(g);
-        //    }
-
-        //    // Refrescar PictureBox
-        //    pictureBox1.Image = canvas;
-        //}
-
-        private void DibujarCuadricula()
-        {
-            using (Graphics g = Graphics.FromImage(canvas))
-            { 
-                int width = pictureBox1.Width;
-                int height = pictureBox1.Height;
-                int cellWidth = width / 3;
-                int cellHeight = height / 3;
-
-                using (Pen gridPen = new Pen(Color.Black, 2))
-                {
-                    // Líneas verticales internas
-                    g.DrawLine(gridPen, cellWidth, 0, cellWidth, height);
-                    g.DrawLine(gridPen, 2 * cellWidth, 0, 2 * cellWidth, height);
-                    // Líneas horizontales internas
-                    g.DrawLine(gridPen, 0, cellHeight, width, cellHeight);
-                    g.DrawLine(gridPen, 0, 2 * cellHeight, width, 2 * cellHeight);
-                }
-            
-            }
-        }
-
-        private void DibujarPuntos(Graphics g)
-        {
-            foreach (var punto in puntos)
-            {
-                g.FillEllipse(Brushes.Red, punto.X - 2, punto.Y - 2, 4, 4);
-            }
-        }
-
-        private void DibujarLinea(Graphics g)
-        {
-            if (puntos.Count == 3)
-            {
-                using (Pen linePen = new Pen(Color.Blue, 2))
-                {
-                    g.DrawLine(linePen, puntos[1], puntos[2]);
-                }
-            }
+            pictureBox1.Invalidate();
         }
     }
 }
